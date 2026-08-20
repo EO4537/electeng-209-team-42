5c5994d25dfd7920674f51325fdd5a86bbc202d2
Aditya Thapliyal <atha725@aucklanduni.ac.nz>
2026-08-20 17:30:05 +1200

    Adding Microchip Folder

---
+++ Microchip/UART Implementation/UART Implementation/main.c
@@ -0,0 +1,18 @@
+/*
+ * ProgressReview.c
+ *
+ * Created: 20/08/2026 5:22:34 pm
+ * Author : adity
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
